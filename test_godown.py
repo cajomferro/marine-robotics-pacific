ddd656c0db904bb5c2b007685782b1cd360c1c4e
Carlos Mão de Ferro <cajomferro@gmail.com>
2021-07-29 17:59:20 +0100

    data from pool

--- test_godown.py
+++ test_godown.py
@@ -11,21 +11,34 @@ from pacific.servos import Syringe
 
 @dataclass
 class Runner:
-    init_wait_time_sec: int = 20
-    path: Path = Path("out.csv")
+    init_wait_time_sec: int = 15
+    path: Path = None
+    syringe_sec: int = 14
+    trip_dur_sec: int = 100
 
     def print(self, msg: str):
         now = datetime.now()  # current date and time
-        output = f"{now.strftime('%H:%M:%S')}, {msg}\n"
+        output = f"{now.strftime('%H:%M:%S')}; {msg}\n"
         with self.path.open("a+") as fd:
             fd.write(output)
         print(output)
 
     def __post_init__(self):
+        now = datetime.now()  # current date and time
+        self.path = Path(f"out_{now.strftime('%H_%M_%S')}.csv")
         self.print("Boot")
         self.pitch_roll = IMUSensorFusion()
         self.pressure = Pressure()
         self.syringe = Syringe()
+        self.remove_air()
+
+    def remove_air(self):
+        # self.print("Removing air")
+        # for i in range(1, 2):
+        #     self.syringe.open()  # open linear act, close syringe
+        #     time.sleep(self.syringe_sec)
+        #     self.syringe.close()  # open linear act, close syringe
+        #     time.sleep(self.syringe_sec)
         self.syringe.open()  # open linear act, close syringe
         self.print(f"Waiting {self.init_wait_time_sec} seconds")
         time.sleep(self.init_wait_time_sec)
@@ -35,25 +48,29 @@ class Runner:
         self.syringe.close()  # close linear act, open syringe
 
         counter = 0
-
-        while counter <= 100:
+        while counter <= self.trip_dur_sec:
             p, r = self.pitch_roll.read()
             press, temp = self.pressure.read()
 
+            if press > 1.05:
+                break
+
             # self.print(f"Pitch: {p} | Roll: {r} | Press: {press} | Temp: {temp}")
-            self.print(f"{p}, {r}, {press}, {temp}")
+            self.print(f"{p}; {r}; {press}; {temp}")
             time.sleep(0.5)
 
             counter += 1
 
         self.syringe.open()  # open linear act, open syringe
-        time.sleep(10)
+        time.sleep(self.syringe_sec)
 
 
 if __name__ == '__main__':
     try:
         r = Runner()
-        r.run()
+        for i in range(1, 5):
+            time.sleep(8)
+            r.run()
     except (KeyboardInterrupt, SystemExit) as exErr:
         print("\nEnding Example 1")
         sys.exit(0)
